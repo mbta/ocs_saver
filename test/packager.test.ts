23711641a66ca095d05e051004b89a3929700431
Cora Grant <394835+x@users.noreply.github.com>
2022-02-16 16:02:54 -0500

    feat: implement no-op packager Lambda

--- test/packager.test.ts
+++ test/packager.test.ts
@@ -1,8 +1,20 @@
-import hello from "../src/packager";
+import { handler } from "../src/packager";
+import { ScheduledEvent } from "aws-lambda";
+import { callback, context } from "./fixtures";
 
-describe("hello", () => {
-  test("says hello", () => {
-    const greeting = hello("world");
-    expect(greeting).toEqual("Hello world!");
-  });
+const event: ScheduledEvent = {
+  version: "0",
+  account: "1234567890",
+  region: "us-east-1",
+  detail: {},
+  "detail-type": "Scheduled Event",
+  source: "aws.events",
+  time: "2019-03-01T01:23:45Z",
+  id: "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
+  resources: ["arn:aws:events:..."],
+};
+
+test("does nothing", async () => {
+  const result = await handler(event, context, callback);
+  expect(result).toBeUndefined();
 });
