5073202b64f8fb67a628c22845352172a3cd7fd0
Cora Grant <394835+x@users.noreply.github.com>
2022-02-17 15:31:51 -0500

    feat: implement no-op processor Lambda

--- test/processor.test.ts
+++ test/processor.test.ts
@@ -1,8 +1,33 @@
-import hello from "../src/processor";
+import { handler } from "../src/processor";
+import { FirehoseTransformationEvent } from "aws-lambda";
+import { callback, context } from "./fixtures";
 
-describe("hello", () => {
-  test("says hello", () => {
-    const greeting = hello("world");
-    expect(greeting).toEqual("Hello world!");
+const event: FirehoseTransformationEvent = {
+  invocationId: "abc12345",
+  deliveryStreamArn: "aws:arn:firehose:...",
+  sourceKinesisStreamArn: "aws:arn:kinesis:...",
+  region: "us-east-1",
+  records: [
+    {
+      recordId: "xyz12345",
+      approximateArrivalTimestamp: 1645129543,
+      data: "VGhlIGZpc2ggd2FzIGRlbGlzaCwgYW5kIGl0IG1hZGUgcXVpdGUgYSBkaXNoLg==",
+    },
+  ],
+};
+
+test("passes records through with a static partition key", async () => {
+  const { recordId, data } = event.records[0];
+  const result = await handler(event, context, callback);
+
+  expect(result).toEqual({
+    records: [
+      {
+        recordId,
+        data,
+        result: "Ok",
+        metadata: { partitionKeys: { serviceDay: "1970-01-01" } },
+      },
+    ],
   });
 });
