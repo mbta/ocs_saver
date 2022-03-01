bdb37afa12cbb740aa9971b31a5a6cd482431324
Cora Grant <394835+x@users.noreply.github.com>
2022-03-01 15:38:05 -0500

    dev: enable imports relative to project root

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  moduleNameMapper: { "^~/(.*)$": "<rootDir>/$1" },
   preset: "ts-jest",
   testEnvironment: "node",
 };
